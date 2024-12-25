eb0e1f6d4b987e890fa8771b7385af081c7609e0
DGDays (Олег Париев) <oleg.pariev@gmail.com>
2024-12-25 08:06:00 +0500

    Теперь происходит вход в аккаунт и выкачивание таблицы

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import eel
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from time import sleep
 
 @eel.expose
 def get_lessons(login, password):
     driver = webdriver.Firefox()
+    table = None
     try:
         driver.get("https://studlk.susu.ru")
+        driver.find_element(By.ID, "UserName_I").send_keys(login)
+        driver.find_element(By.ID, "dxPassword_I").send_keys(password)
+        driver.find_element(By.ID, "LoginBtn").click()
+
+        driver.get("https://studlk.susu.ru/ru/StudyPlan/StudyPlanView")
+        try:
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.ID, "gvStudyPlan_tccell1_1"))
+            )
+        except:
+            pass
+        table = driver.find_element(By.ID, "gvStudyPlan_DXMainTable").get_attribute("outerHTML")
     finally:
         driver.quit()
-    return True
+    return table
 
 eel.init("./other/gui")
 
